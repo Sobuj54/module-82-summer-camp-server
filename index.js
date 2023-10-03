380e13704db77d771bd5fec6e2f323c1c7006e25
soubj <sobujahmed124@gmail.com>
2023-10-03 20:19:23 +0600

    updated user role

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -60,6 +60,18 @@ async function run() {
 
     app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.patch("/users", async (req, res) => {
+      const updatedRole = req.body;
+      const filter = { _id: new ObjectId(updatedRole.id) };
+      const updatedUser = {
+        $set: {
+          role: updatedRole.role,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedUser);
       res.send(result);
     });
 
